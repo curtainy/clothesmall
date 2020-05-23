4ee53bde537564d1365d2fb9ef27dd9848113fa8
wy <3554723391@qq.com>
2020-05-23 16:13:23 +0800

    使用beforRoute隐藏main-tab-bar

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <keep-alive exclude="Detail">
       <router-view/>
     </keep-alive>
-    <main-tab-bar/>
+    <main-tab-bar :class="{hidden: isHidden}"/>
   </div>
 </template>
 
@@ -12,12 +12,32 @@ import MainTabBar from 'components/content/mainTabbar/MainTabBar'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      isHidden: false
+    }
+  },
   components: {
     MainTabBar
+  },
+  mounted(){
+    // 在登陆注册界面隐藏<main-tab-bar/>
+    this.$router.beforeEach((to,from,next)=>{
+      // console.log(to)
+      if(to.path == '/login' || to.path == '/register'){
+        this.isHidden = true
+      }else{
+        this.isHidden = false
+      }
+      next()
+    })
   }
 }
 </script>
 
 <style>
   @import "assets/css/base.css";
+  .hidden{
+    visibility: hidden;
+  }
 </style>
